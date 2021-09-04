ac68e61d7b4e210a7e0776ab6d555b1e3f337a74
Nephael <67867993+Nephael@users.noreply.github.com>
2021-09-05 00:23:34 +0900

    Add files via upload

---
+++ src/구조체의 메모리의 크기를 확인(소스).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct list { //list라는 구조체를 정의한다. (list는 변수가 아니라 키워드이다.)
+    char name; // 문자 배열형 멤버 (1바이트)
+    int age; // 정수형 멤버 (4바이트)
+};
+
+int main(void) {
+    struct list st1 = {'Nephael', 20}; 
+
+    printf("구조체 list의 크기는 %d이다. \n", sizeof(struct list));
+    printf("구조체 객체 st1의 크기는 %d이다. \n", sizeof(st1));
+
+    return 0;
+}
