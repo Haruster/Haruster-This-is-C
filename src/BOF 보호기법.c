6bbcff02594dfc2260fca6147e1d4089793f5b72
Nephael <67867993+Nephael@users.noreply.github.com>
2021-09-07 01:13:08 +0900

    Create BOF 보호기법.c

---
+++ src/BOF 보호기법.c
@@ -0,0 +1,16 @@
+//BOF보호기법.c
+#include <stdio.h>
+
+int x = 1; //data
+
+int main()
+{
+int i; //stack
+int y; //stack
+static int k; //bss
+static int j=1; //data
+char *c; //stack
+c = malloc(10); //heap
+y = 2; //code
+return 0; //stack
+}
