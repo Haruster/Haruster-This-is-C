e656a97f05b7e5a7ef3ce16b664992bf456a121f
Phantester S <67867993+Apasys@users.noreply.github.com>
2021-11-17 21:51:09 +0900

    Add files via upload

---
+++ printf 문자열 출력.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+    printf("My Age : %d \n", 20);
+    printf("%d is my poin \n", 100);
+    printf("Good \n Morning \n EveryBody\n");
+
+    return 0;
+
+}
