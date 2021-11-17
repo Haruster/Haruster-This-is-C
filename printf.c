51c22c595ccd255de7ca5a5e5922c069661656b1
Phantester S <67867993+Apasys@users.noreply.github.com>
2021-11-17 21:51:18 +0900

    Add files via upload

---
+++ printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Hello EveryBody\n"); // \n : 한 줄 건너뛰기
+    printf("%d \n", 1234); // %d : 10진수 출력 (1234를 출력)
+    printf("%d %d\n", 10, 20);
+
+    return 0;
+
+}
