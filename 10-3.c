bbb2ae8df970db7ae9846f636f669af9d809ec4e
Phantester S <67867993+Apasys@users.noreply.github.com>
2021-11-17 21:34:06 +0900

    Add files via upload

---
+++ 10-3.c
@@ -0,0 +1,32 @@
+// 포인터를 사용하지 않고 두 수 바꾸기
+
+#include <stdio.h>
+
+void Swap(int a, int b);
+
+int main(void) {
+
+    int x = 10, y = 5;
+
+    printf("Swap() 함수 실행 전\n");
+
+    printf("x = %d, y = %d이다. \n\n", x, y);
+
+    Swap(x, y);
+
+    printf("Swap() 함수 실행 후\n");
+
+    printf("x = %d, y = %d이다. \n\n", x, y);
+
+    return 0;
+
+}
+
+void Swap(int a, int b) {
+
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+
+}
