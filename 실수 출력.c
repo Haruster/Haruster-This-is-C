a8c9471185a0c2c5ea5dd594fd270da53b5916ff
Phantester S <67867993+Apasys@users.noreply.github.com>
2021-11-17 21:27:45 +0900

    Add files via upload

---
+++ 실수 출력.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    float x = 10.12; //실수형 변수 x에 10.12라는 값을 저장함.
+    double y = 10.1232; // 실수형 변수 y에 10.1232라는 값을 저장함.
+
+    printf("%f %lf \n", x, y);
+
+    return 0;
+
+}
