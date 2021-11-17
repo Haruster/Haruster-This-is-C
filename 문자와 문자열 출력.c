3c72d7136df44f5de661133e9ae31b9c4a07466f
Phantester S <67867993+Apasys@users.noreply.github.com>
2021-11-17 21:27:35 +0900

    Add files via upload

---
+++ 문자와 문자열 출력.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    printf("%c %s\n", 'A', "Hello World");
+
+    return 0;
+    
+}
+
+//한 개의 문자를 사용하기 위해서는 %c를 사용하며 ''를 사용하고 문자열을 출력하기 위해서는 %s를 사용하고 ""을 사용한다.
