f868df79439176f9ee75bda36ed652377bf0f7b5
Phantester S <67867993+Apasys@users.noreply.github.com>
2021-11-17 21:49:28 +0900

    Add files via upload

---
+++ 12-7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define PI 3.14
+
+int main(void) {
+    int R;
+
+    float cir;
+
+    printf("원의 반지름을 입력하세요 : ");
+
+    scanf("%d", &R);
+
+    cir = PI * (2 * R);
+
+    printf("원의 둘레는 %.2f입니다.\n", cir);
+
+    return 0;
+}
