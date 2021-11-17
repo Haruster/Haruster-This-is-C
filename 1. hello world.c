b0bb9a96dc01e4ccd63e36543f34ca175deca7c2
Phantester S <67867993+Apasys@users.noreply.github.com>
2021-11-17 21:27:54 +0900

    Add files via upload

--- 1. hello world.c
+++ 1. hello world.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("hello world\n");
+
+    printf("Hello World\n");
 
     return 0;
+
 }
+
+// int main(void)는 int형으로 main함수를 열게 되는데 이때는 반환값으로 정수값을 넣어줘야 하므로 return 0;를 이용해서 종료값을 반환해준다.
