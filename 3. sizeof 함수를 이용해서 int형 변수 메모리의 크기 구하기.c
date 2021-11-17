4623076bbffff2fe0e370c4aed5f69ce76d18cc2
Phantester S <67867993+Apasys@users.noreply.github.com>
2021-11-17 21:29:22 +0900

    Add files via upload

---
+++ 3. sizeof 함수를 이용해서 int형 변수 메모리의 크기 구하기.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int x = 5;
+
+    printf("변수 x의 메모리 크기는 %d입니다.", sizeof(x)); //sizeof() 함수를 사용해서 메모리의 크기를 확인할 수 있다.
+
+    return 0;
+
+}
+
+// int형 메모리 크기는 4바이트이다.
