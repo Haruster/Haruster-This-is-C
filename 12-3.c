a77c6af87485ce2b1fed9f9f9d0684116b008c29
Phantester S <67867993+Apasys@users.noreply.github.com>
2021-11-17 21:48:49 +0900

    Add files via upload

---
+++ 12-3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    FILE* fp;
+
+    int n = 4000;
+
+    if((fp = fopen("basic.txt", "wb")) == NULL) {
+
+        printf("파일을 열 수 없습니다.");
+        exit(1);
+
+    }
+
+    fwrite(&n, sizeof(int), 1, fp);
+
+    fclose(fp);
+
+    return 0;
+
+}
