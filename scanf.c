91ee321fa94b4728d1761bb3508cb2adb7e12aa6
Phantester S <67867993+Apasys@users.noreply.github.com>
2021-11-17 21:54:03 +0900

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int number1, number2;
+
+    printf("number1, number2의 값을 입력해주세요 : \n");
+
+    scanf("%d %d", &number1, &number2);
+
+    printf("number1의 값은 %d, number2의 값은 %d입니다.\n", number1, number2);
+
+    return 0;
+
+}
