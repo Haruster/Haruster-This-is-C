b5f53dcb1b6785dcf3d186aab3b52ce5986f538f
Phantester S <67867993+Apasys@users.noreply.github.com>
2021-11-17 21:30:11 +0900

    Add files via upload

---
+++ 6. 사칙연산.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int a = 10;
+    int b = 20;
+
+    int one = a + b;
+    int two = a - b;
+    int three = a * b;
+    int four = a / b;
+
+    printf("a + b = %d\n", one);
+    printf("a - b = %d\n", two);
+    printf("a * b = %d\n", three);
+    printf("a / b = %d\n", four);
+
+    return 0;
+
+
+}
