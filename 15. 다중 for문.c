dc46b99c45ebe754edfed458b7a75cac449f261e
Phantester S <67867993+Apasys@users.noreply.github.com>
2021-11-17 21:32:46 +0900

    Add files via upload

---
+++ 15. 다중 for문.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int i;
+    int j;
+
+    for (i = 0; i < 5; i++) {
+
+        for (j = 1; j < 6; j++) {
+
+            printf("*");
+
+        }
+
+        printf("\n");
+
+    }
+
+    return 0;
+
+}
