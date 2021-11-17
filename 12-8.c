40390a9f7545d80e3ce62eaf14fc8d2df55fabf8
Phantester S <67867993+Apasys@users.noreply.github.com>
2021-11-17 21:49:37 +0900

    Add files via upload

---
+++ 12-8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define PI 3.14
+#define AREA(X) (PI * (X) * (X))
+
+int main(void) {
+    
+    float R;
+
+    printf("원의 반지름을 입력하세요 : ");
+
+    scanf("%f", &R);
+
+    printf("원의 면적은 %.2f입니다. \n", AREA(A));
+
+    return 0;
+    
+}
